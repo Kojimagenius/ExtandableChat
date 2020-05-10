c5263481ca4e2d79f7c13fa77e0276b078c2168d
Max <dun.max@yandex.ru>
2020-05-10 16:58:04 +0300

    reformat view

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,42 +1,85 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth import get_user_model
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
+from .models import Message
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+User = get_user_model()
 
-    async def disconnect(self, close_code):
+
+class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = Message.last_10_messages()
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        author = data['from']
+        author_user = User.objects.filter(username = author)[0]
+        message = Message.objects.create(author=author_user, content=data['message'])
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    commands = {
+        'fetch_messages' : fetch_messages,
+        'new_message' : new_message
+    }
+
+    def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    async def connect(self):
+    def connect(self):
+        #join room group
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+        self.accept()
 
-        await self.accept()
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        await self.channel_layer.group_send(self.room_group_name,{
+    def send_chat_message(self, message):
+        async_to_sync( self.channel_layer.group_send)(
+            self.room_group_name,
+            {
             'type':'chat_message',
             'message': message
-        }
+            }
                                             )
 
-    async def chat_message(self, event):
-        #receive message from chat room func
-        message = event['message']
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
 
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
--- Chat/views.py
+++ Chat/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
+import json
 
 
 def index(request):
     return render(request, 'Chat/index.html', {})
 
+
+@login_required
 def room(request, room_name):
-    return render(request, "Chat/room.html", {"room_name": room_name})
+    return render(request, "Chat/room.html", {
+        'room_name': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username))
+    })
 
 # Create your views here.
